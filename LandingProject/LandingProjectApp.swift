366ca9ec912a38b18ff9857f3bdae08e468c3a10
Farhan Sadik Galib <farhansadikgalib@gmail.com>
2023-01-10 10:32:12 +0600

    Initial Commit

---
+++ LandingProject/LandingProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandingProjectApp.swift
+//  LandingProject
+//
+//  Created by Farhan Sadik on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct LandingProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
